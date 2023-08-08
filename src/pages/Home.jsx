660c1b403e2ebe4fefbd7789a69055155c861f64
dlwhsk0 <hana8145@gmail.com>
2023-08-08 17:25:24 +0900

    2023-08-08 17:23

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../component/Header";
+
+export default function Home() {
+  return (
+    <div className="homeWrap">
+      <Header />
+      <div className="info-wrap">
+        <div className="info">
+          <p style={{ height: "20px" }}></p>
+          <p className="title">iLuzen을 찾아주셔서 감사합니다.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
