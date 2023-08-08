9fd68eb43a7beac08a5878005752356a90c57190
dlwhsk0 <hana8145@gmail.com>
2023-08-08 17:28:43 +0900

    2023-08-08 17:28

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -9,15 +9,12 @@ export default function Footer() {
         <p style={{ height: "20px" }}></p>
         <div>
           <ul className="footer-list">
-            <li>회사명: JH시스템</li>
-            <li>대표자: 최재호</li>
+            <li>회사명: iLuzen</li>
+            <li>대표자: 이은지</li>
             <li>이메일 : iLuzen@iLuzen.com</li>
             <li>전화 : 010-1234-5678</li>
             <li>사업자등록번호 : 190-12-34567</li>
-            <li>
-              주소 : 경기 수원시 팔달구 갓매산로 51 (에스알프라자23차) 6층 601
-              코스팟 (23호JH시스템)
-            </li>
+            <li>주소 : 서초구 남부순환로 333길 10 (파이브스팟) 3층</li>
           </ul>
         </div>
       </div>
