1c40057c83a748bf9564f7efa67d172e117dd7e6
dlwhsk0 <hana8145@gmail.com>
2023-09-04 21:14:09 +0900

    페이지 이동 시 맨 위로 올라가기 구현

--- src/AppMain2.jsx
+++ src/AppMain2.jsx
@@ -5,11 +5,13 @@ import HeaderNoMove from "./component/HeaderNoMove";
 import HomeMain from "./pages/HomeMain";
 import Footer from "./component/Footer";
 import Quate from "./pages/Quate";
+import ScrollToTop from "./component/ScrollToTop";
 
 export default function AppMain2() {
   return (
     <div>
       <BrowserRouter>
+        <ScrollToTop />
         <HeaderNoMove />
         <Routes>
           <Route path="/" element={<HomeMain />}></Route>
