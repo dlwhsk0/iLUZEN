04a1a76d9cdd4b21c9592c1c1833bfe7236614dc
dlwhsk0 <hana8145@gmail.com>
2023-09-04 21:22:59 +0900

    ScrollToTop 오류

--- src/AppMain2.jsx
+++ src/AppMain2.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./AppMain2.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ScrollToTop from "./component/ScrollToTop";
 import HeaderNoMove from "./component/HeaderNoMove";
 import HomeMain from "./pages/HomeMain";
 import Footer from "./component/Footer";
 import Quate from "./pages/Quate";
-import ScrollToTop from "./component/ScrollToTop";
 
 export default function AppMain2() {
   return (
