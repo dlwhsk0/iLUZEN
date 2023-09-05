a4920f837bdfca6c0c756e4f9c086c894b0681b9
dlwhsk0 <hana8145@gmail.com>
2023-09-05 12:22:41 +0900

    iLuzen 1차 파일 public으로 이동

---
+++ public/iLuzenVer1/AppMain.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./AppMain.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "../../src/component/Footer";
+import About from "./pages/About";
+import Service from "./pages/Service";
+import Project from "./pages/Project";
+import News from "./pages/News";
+import Contact from "../../src/pages/Contact";
+import Home from "./pages/Home";
+
+export default function AppMain() {
+  return (
+    <div className="wrap">
+      <BrowserRouter>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/pages/about" element={<About />}></Route>
+            <Route path="/pages/service" element={<Service />}></Route>
+            <Route path="/pages/project" element={<Project />}></Route>
+            <Route path="/pages/news" element={<News />}></Route>
+            <Route path="/pages/contact" element={<Contact />}></Route>
+          </Routes>
+        </main>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
---
+++ public/iLuzenVer1/pages/Service.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../../src/component/Header";
+
+export default function Service() {
+  return (
+    <div className="serviceWrap">
+      <div className="top">
+        <Header />
+      </div>
+      <h1>서비스 탭입니다.</h1>
+    </div>
+  );
+}
