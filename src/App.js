523cb90aedd64e161d8277c5d8a0f7b3f3b37efa
dlwhsk0 <hana8145@gmail.com>
2023-09-05 12:21:02 +0900

    HEADER 스크롤 핸들러 구현

--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
-// App.js
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./AppMain.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ScrollToTop from "./component/ScrollToTop";
+import Header from "./component/Header";
+import Home from "./pages/Home";
+import Footer from "./component/Footer";
+import Quate from "./pages/Quate";
 
-const Home = () => <h1>홈 페이지</h1>;
-const About = () => <h1>소개 페이지</h1>;
-const Contact = () => <h1>연락 페이지</h1>;
-
-const App = () => (
-  <Router>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">홈</Link>
-        </li>
-        <li>
-          <Link to="/about">소개</Link>
-        </li>
-        <li>
-          <Link to="/contact">연락</Link>
-        </li>
-      </ul>
-    </nav>
-
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
-  </Router>
-);
-
-export default App;
+export default function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <ScrollToTop />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/pages/quate" element={<Quate />}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
--- src/AppMain.jsx
+++ src/AppMain.jsx
@@ -1,28 +1,22 @@
 import React from "react";
 import "./AppMain.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Footer from "./component/Footer";
-import About from "./pages/About";
-import Service from "./pages/Service";
-import Project from "./pages/Project";
-import News from "./pages/News";
-import Contact from "./pages/Contact";
+import ScrollToTop from "./component/ScrollToTop";
+import Header from "./component/Header";
 import Home from "./pages/Home";
+import Footer from "./component/Footer";
+import Quate from "./pages/Quate";
 
 export default function AppMain() {
   return (
-    <div className="wrap">
+    <div>
       <BrowserRouter>
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/pages/about" element={<About />}></Route>
-            <Route path="/pages/service" element={<Service />}></Route>
-            <Route path="/pages/project" element={<Project />}></Route>
-            <Route path="/pages/news" element={<News />}></Route>
-            <Route path="/pages/contact" element={<Contact />}></Route>
-          </Routes>
-        </main>
+        <ScrollToTop />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/pages/quate" element={<Quate />}></Route>
+        </Routes>
         <Footer />
       </BrowserRouter>
     </div>
