5a6c256291bc451872b51400f48661285acfb023
dlwhsk0 <hana8145@gmail.com>
2023-09-27 03:07:03 +0900

    2차: 레이아웃

--- src/AppMain.jsx
+++ src/AppMain.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./AppMain.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ScrollToTop from "./component/ScrollToTop";
 import Header from "./component/Header";
 import Home from "./pages/Home";
 import Footer from "./component/Footer";
@@ -11,7 +10,6 @@ export default function AppMain() {
   return (
     <div>
       <BrowserRouter>
-        <ScrollToTop />
         <Header />
         <Routes>
           <Route path="/" element={<Home />}></Route>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,100 +1,97 @@
 import React from "react";
-import pipe from "../image/pipesupport.jpg";
-import dong from "../image/systemdongba.jpg";
-import bigae from "../image/systembigae.jpg";
-import gang from "../image/ganggwan.jpg";
-import external from "../image/externallink.png";
-import { Link } from "react-router-dom";
+import "../styles/Home.css";
+import contact1 from "../images/contact1.png";
+import contact2 from "../images/contact2.png";
 
 export default function Home() {
   return (
     <>
       <main>
-        <div className="section">
-          <div className="sectionwrap center">
-            <div className="title">
-              <h1>
-                Building the reality,
-                <br />
-                connecting the future.
-              </h1>
-            </div>
-          </div>
-        </div>
-        <div className="section">
-          <div className="sectionwrap">
-            <div className="content">
-              <h1 className="content-title">"건설 현장의 혁신과 열정"</h1>
-              <p style={{ height: "15px" }}></p>
-              <p>
-                iLuzen은 건설 분야 중 하나인 가설구조물 설계 전문 기업입니다.
-                가설구조물 중 시스템 동바리, 시스템 비계를 주로 설계하고
-                있습니다.
-              </p>
-              <p>
-                오랜 경험과 전문성을 토대로 고객의 다양한 건설 요구에 부응하며,
-                건설 현장에서의 정확한 계획과 실행을 통해 프로젝트를 성공적으로
-                완수하고, 고객의 신뢰를 얻어내는 일에 헌신하고 있습니다.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="section">
-          <div className="sectionwrap">
-            <div className="product">
-              <div className="productleft">
-                <span className="producttitle">설계 품목</span>
-                <div className="bar"></div>
+        <div className="mainWrap">
+          <section>
+            <div className="sectionWrap">
+              <div className="titleWrap">
+                <h1 className="mainTitle">
+                  Building the reality,
+                  <br />
+                  connecting the future.
+                </h1>
+                <h2 className="subTitle">
+                  iLuzen은 건설 분야 중 하나인 가설구조물 설계 전문 기업입니다.
+                  가설구조물 중 시스템 동바리, 시스템 비계를 주로 설계하고
+                  있습니다.
+                  <br />
+                  <br />
+                  오랜 경험과 전문성을 토대로 고객의 다양한 건설 요구에
+                  부응하며, 건설 현장에서의 정확한 계획과 실행을 통해 프로젝트를
+                  성공적으로 완수하고, 고객의 신뢰를 얻어내는 일에 헌신하고
+                  있습니다.
+                </h2>
               </div>
-              <div className="productright">
-                <div>
-                  <ul className="productlist">
-                    <li className="productitem">
-                      <img src={pipe} alt="파이프써포트" />
-                      <p>파이프써포트</p>
+              <div className="imageWrap">
+                <ul>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                </ul>
+              </div>
+            </div>
+          </section>
+          <section>
+            <div className="sectionWrap">sec2</div>
+          </section>
+          <section>
+            <div className="sectionWrap">
+              <div className="imageBox">
+                <div className="imageBox-img"></div>
+                <div className="imageBox-cont">
+                  <h1>설계 품목</h1>
+                  <ul>
+                    <li>
+                      <span></span>
+                      <img src="" alt="" />
                     </li>
-                    <li className="productitem">
-                      <img src={dong} alt="시스템동바리" />
-                      <p>시스템동바리</p>
+                    <li>
+                      <span></span>
+                      <img src="" alt="" />
                     </li>
-                    <li className="productitem">
-                      <img src={bigae} alt="시스템비계" />
-                      <p>시스템비계</p>
+                    <li>
+                      <span></span>
+                      <img src="" alt="" />
                     </li>
-                    <li className="productitem">
-                      <img src={gang} alt="강관비계" />
-                      <p>강관비계</p>
+                    <li>
+                      <span></span>
+                      <img src="" alt="" />
                     </li>
                   </ul>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div className="section">
-          <div className="sectionwrap">
-            <div className="quatewrap">
-              <div className="quate">
-                <h1 className="quate-title">ONLINE QUATE CONTACT</h1>
-                <p style={{ height: "15px" }}></p>
-                <p className="quate-text">
-                  iLuzen은 온라인 견적 기능을 제공합니다.
-                </p>
-                <div>
-                  <Link to="/pages/quate">
-                    <div className="quate-btn">
-                      <p>문의하기</p>
-                      <img
-                        className="quate-btn-img"
-                        src={external}
-                        alt="외부링크이동"
-                      />
-                    </div>
-                  </Link>
+          </section>
+          <section>
+            <div className="sectionWrap contactWrap">
+              <h1>ONLINE QUATE CONTACT</h1>
+              <div className="contactBox">
+                <div className="contact">
+                  <img
+                    src={contact1}
+                    alt="작업의뢰"
+                    style={{ width: "65px", margin: "0 auto" }}
+                  />
+                  <h1>작업의뢰</h1>
+                </div>
+                <div className="contact">
+                  <img
+                    src={contact2}
+                    alt="채용문의"
+                    style={{ width: "100px", margin: "0 auto" }}
+                  />
+                  <h1>채용문의</h1>
                 </div>
               </div>
             </div>
-          </div>
+          </section>
         </div>
       </main>
     </>
