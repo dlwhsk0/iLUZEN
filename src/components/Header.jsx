46b06b372a07fba3a41f5f6f999aa91e0160582b
dlwhsk0 <hana.dev.314@gmail.com>
2024-08-23 20:56:55 +0900

    feat: 스크롤에 따른 nav 글씨 색상 변화 기능 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import logo from '../images/iluzen/iluzen-logo.png'
 
 export default function Header() {
@@ -18,6 +18,38 @@ export default function Header() {
     }
   }
 
+  useEffect(() => {
+    const sectionIds = ['회사소개', '업무소개', '설계품목', '고객지원']
+
+    const handleScroll = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setActive(entry.target.id)
+        }
+      })
+    }
+
+    const observer = new IntersectionObserver(handleScroll, {
+      threshold: 0.6, // 섹션이 60% 이상 화면에 보일 때 활성화
+    })
+
+    sectionIds.forEach((id) => {
+      const section = document.querySelector(`#${id}`)
+      if (section) {
+        observer.observe(section)
+      }
+    })
+
+    return () => {
+      sectionIds.forEach((id) => {
+        const section = document.querySelector(`#${id}`)
+        if (section) {
+          observer.unobserve(section)
+        }
+      })
+    }
+  }, [])
+
   return (
     <div
       className='w-full h-[50px] px-[60px] py-0 flex items-center justify-center fixed top-0 left-0 right-0 z-50 bg-white'
@@ -25,7 +57,7 @@ export default function Header() {
     >
       <div className='w-[1280px] min-w-[620px] flex items-center justify-between'>
         <img src={logo} className='w-[150px]' alt='iluzen logo' />
-        <div className='w-[40%] min-w-[350px] text-[16px] flex items-center justify-between'>
+        <nav className='w-[40%] min-w-[350px] text-[16px] flex items-center justify-between'>
           <span
             className={`cursor-pointer ${active === '회사소개' ? 'text-[#002970] font-bold' : ''}`}
             onClick={() => handleClick('회사소개')}
@@ -50,7 +82,7 @@ export default function Header() {
           >
             고객지원
           </span>
-        </div>
+        </nav>
       </div>
     </div>
   )
