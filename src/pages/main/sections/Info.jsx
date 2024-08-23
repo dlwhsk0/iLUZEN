e30de4db85235e1834b56d3d7dd98446274ffc95
dlwhsk0 <hana.dev.314@gmail.com>
2024-08-23 20:26:25 +0900

    fix: build 에러 해결
    eslint: img 태그에 alt 속성 없음 해결

--- src/pages/main/sections/Info.jsx
+++ src/pages/main/sections/Info.jsx
@@ -36,7 +36,7 @@ export default function Info() {
     >
       <div className='w-[1280px] min-w-[620px] flex flex-col items-center justify-center gap-5'>
         <div className='w-[1280px] flex justify-center gap-3'>
-          <img src={iluzenBlue} className='h-[50px]' />
+          <img src={iluzenBlue} className='h-[50px]' alt='' />
           <span className='text-[#002970] text-[30px]'>|</span>
           <span className='text-[30px] text-gray-800 pt-[2px]'>
             주식회사 이루젠
