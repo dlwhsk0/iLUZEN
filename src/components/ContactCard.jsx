c9e1f4f18b5f3ed2764051d533cbe5785912673c
dlwhsk0 <hana.dev.314@gmail.com>
2024-09-04 21:15:57 +0900

    design: contact 반응형 디자인 수정

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,17 +1,19 @@
 export default function ContactCard({ icon, content, onClick }) {
   return (
     <div
-      className='w-full sm:w-[80%] max-w-[350px] h-[60px] sm:h-[90px] text-[20px] flex items-center justify-center border border-black bg-white cursor-pointer'
+      className='w-full sm:w-[80%] max-w-[350px] h-[60px] sm:h-[90px] flex items-center justify-center border border-black bg-white cursor-pointer'
       onClick={onClick}
     >
-      <div className='w-[80%]'>
-        <div className='w-full max-w-[120px] sm:w-[70%] flex items-center justify-between'>
+      <div className='w-[80%] sm:w-[70%]'>
+        <div className='w-full max-w-[120px] flex items-center'>
           <img
             src={icon}
-            className='h-[20%] max-h-[35px] object-contain'
+            className='h-[20%] max-h-[35px] object-contain mr-4 sm:mr-5'
             alt=''
           />
-          <span>{content}</span>
+          <span className='text-[16px] sm:text-[20px] whitespace-nowrap'>
+            {content}
+          </span>
         </div>
       </div>
     </div>
