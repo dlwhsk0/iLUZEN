98148534d22c3c19c516b494c229ca23399ec176
dlwhsk0 <hana.dev.314@gmail.com>
2025-04-15 12:01:26 +0900

    design: 설계 품목 ItemCard 애니메이션 수정

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types'
 export default function ItemCard({ src, title, content }) {
   return (
     <div className='relative w-full h-full flex items-center justify-center rounded-lg overflow-hidden group lg:h-full md:h-[400px] sm:h-[300px] transition-all duration-[800ms]'>
-      <div className='absolute inset-0 bg-gradient-to-t from-transparent to-black opacity-60 transition-all duration-300'></div>
+      <div className='absolute inset-0 bg-gradient-to-t from-transparent to-black opacity-60 transition-all duration-[800ms]'></div>
 
       <img
         src={src}
         alt={title}
-        className='w-full h-full object-cover'
+        className='w-full h-full object-cover transition-all duration-[800ms] group-hover:scale-110'
         loading='lazy'
         width='600'
         height='400'
@@ -16,7 +16,7 @@ export default function ItemCard({ src, title, content }) {
       <div
         className={`absolute z-10 w-full h-full px-4 lg:py-[10%] py-[8%] flex flex-col gap-2 group-hover:justify-center items-center text-white transition-all duration-[800ms]`}
       >
-        <span className='font-bold md:text-[20px] sm:text-[18px] text-[15px] transition-all duration-[800ms]'>
+        <span className='font-bold md:text-[20px] sm:text-[18px] text-[15px] transition-all duration-[800ms] group-hover:text-[24px]'>
           {title}
         </span>
         <span
